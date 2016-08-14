ecb877f3739ac81fdf09027c5b9b6a7aa49de5ee
hypatiah <danielle.hassid@gmail.com>
2016-08-14 12:39:41 -0700

    Alter spacing to homepage view

--- index.ios.js
+++ index.ios.js
@@ -21,10 +21,11 @@ class AwesomeReach extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          R E A C H out with a simple tap.
+          R E A C H out with a simple tap.{'\n'}
         </Text>
         <Image source={require('./reach_button.png')} style={{width: 300, height: 300}}/>
         <Text style={styles.instructions}>
+          {'\n'}
           Preset SMS messages to send to your support network.
         </Text>
         <Text style={styles.instructions}>
